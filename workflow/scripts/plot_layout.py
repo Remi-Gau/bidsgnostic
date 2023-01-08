090bff8f6aa3b7567b90f71ab2588a3f3bc18ea6
Remi Gau <remi_gau@hotmail.com>
2023-01-08 19:50:40 +0100

    silence group level rule

--- workflow/scripts/plot_layout.py
+++ workflow/scripts/plot_layout.py
@@ -1,6 +1,4 @@
 # pyright: reportUndefinedVariable=false
-# from bidsgnostic.utils import plot_layout
+from bidsgnostic.utils import plot_layout
 
-raise NotImplementedError("Group level is not implemented yet")
-
-# plot_layout(snakemake)
+plot_layout(snakemake)
