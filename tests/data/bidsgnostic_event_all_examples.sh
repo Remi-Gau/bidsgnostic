1608d7e29411c9e441057550173babdbfe6eee2e
Remi Gau <remi_gau@hotmail.com>
2023-01-22 18:01:43 +0100

    paralellize test on all examples

--- tests/data/bidsgnostic_event_all_examples.sh
+++ tests/data/bidsgnostic_event_all_examples.sh
@@ -3,31 +3,7 @@
 set -eux -o pipefail
 
 ################################################################################
-# bidsngostic group
-
-datasets=$(find bids-examples -mindepth 1 -maxdepth 1 -type d)
-
-for dataset in $datasets; do
-
-   if [ -e "bids-examples/.git" ]; then
-       continue
-   fi
-   if [ -e "bids-examples/.github" ]; then
-       continue
-   fi   
-
-    ds_name=$(basename "$dataset")
-
-    : "Running on $dataset"
-
-    mkdir -p ./derivatives/${ds_name}
-
-    bidsgnostic_layout  "$PWD/$dataset" "$PWD/derivatives/${ds_name}" group --plot_by suffix
-
-done
-
-################################################################################
-# bidsngostic participant
+# bidsgnostic participant
 
 datasets=$(find bids-examples -mindepth 1 -maxdepth 1 -type d -name "ds*")
 
---
+++ tests/data/bidsgnostic_layout_all_examples.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+set -eux -o pipefail
+
+################################################################################
+# bidsgnostic group
+
+datasets=$(find bids-examples -mindepth 1 -maxdepth 1 -type d)
+
+DATASET_TO_SKIP=(".github .git")
+
+for dataset in $datasets; do
+
+    ds_name=$(basename "$dataset")
+
+    if [[ " ${DATASET_TO_SKIP[*]} " =~ " ${ds_name} " ]]; then
+        continue    
+    fi    
+
+    : "Running on $dataset"
+
+    mkdir -p ./derivatives/${ds_name}
+
+    bidsgnostic_layout  "$PWD/$dataset" "$PWD/derivatives/${ds_name}" group --plot_by suffix
+
+done
